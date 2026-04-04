f3a057c8bfb316b22f1fe882bca17b045ae2f0be
alantao5056 <alantaolr@outlook.com>
2026-04-04 14:44:26 -0400

    Fix cross-site POST rejection issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,12 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://sat-vocab.alantao.com",
     output: "server",
+    security: {
+        checkOrigin: true,
+        allowedDomains: [{ hostname: "sat-vocab.alantao.com" }],
+    },
     adapter: node({
         mode: "standalone",
     }),
